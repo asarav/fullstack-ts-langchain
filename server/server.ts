d435c3fec976f4e21b76df941cdbefe79a55af02
Avinash Saravanan <asarav@umich.edu>
2025-06-13 15:01:40 -0400

    Make the endpoint functional

--- server/server.ts
+++ server/server.ts
@@ -13,22 +13,24 @@ app.use(express.json());
 
 const llm = new ChatGoogleGenerativeAI({
   model: "gemini-2.0-flash",
-  temperature: 0
+  temperature: 0.5
 });
 
 //TODO: Refactor the types here
-app.post('/chat', async (req: { body: { message: string; }; }, res: any) => {
-  const message = req.body.message;
-  if (!message) {
-    return res.status(400).json({ error: "No message provided" });
-  }
-  try {
-      const response = await llm.invoke([{ role: "user", content: message }]);
-      res.json(response);
-  } catch (error) {
-      console.error("Error during chat:", error);
-      res.status(500).json({ error: "Internal server error" });
-  }
+app.post('/chat', async (req: any, res: any) => {
+    const message = req.body.message;
+    console.log(message)
+    if (!message) {
+        return res.status(400).json({ error: "No message provided" });
+    }
+    try {
+        const response = await llm.invoke([{ role: "user", content: message }]);
+        console.log(response.content)
+        res.json(response);
+    } catch (error) {
+        console.error("Error during chat:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 app.listen(port, () => {
