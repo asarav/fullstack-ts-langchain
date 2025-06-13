fb041e9ea4762f34cad3c4d9e1d0140e6529aba1
Avinash Saravanan <asarav@umich.edu>
2025-06-13 14:38:23 -0400

    Update with next app and gemini endpoint

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,36 @@
-import express from "express";
-import * as dotenv from 'dotenv';
+import express from 'express';
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-//Load environment variables
 dotenv.config();
-
-// Express app
 const app = express();
+const port = process.env.PORT || 3001;
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(express.json());
 
-// Middleware
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
+
+const llm = new ChatGoogleGenerativeAI({
+  model: "gemini-2.0-flash",
+  temperature: 0
 });
 
-// Listen for requests
-app.listen(process.env.PORT, () => console.log(`Server started on port http://localhost:${process.env.PORT}/`));
+//TODO: Refactor the types here
+app.post('/chat', async (req: { body: { message: string; }; }, res: any) => {
+  const message = req.body.message;
+  if (!message) {
+    return res.status(400).json({ error: "No message provided" });
+  }
+  try {
+      const response = await llm.invoke([{ role: "user", content: message }]);
+      res.json(response);
+  } catch (error) {
+      console.error("Error during chat:", error);
+      res.status(500).json({ error: "Internal server error" });
+  }
+});
 
-app.get('/', (req, res) => {
-    res.json({
-        msg: 'Welcome to the app'
-    })
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
